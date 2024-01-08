d2811081d2ddb37f26a995bb109fb8849d4b1db0
Corey Angelus <angelusproductions@gmail.com>
2024-01-08 12:26:27 -0800

    Manually add Nunito to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 import "./globals.scss";
+import "./fonts.scss";
 
 export default function RootLayout({
   children
